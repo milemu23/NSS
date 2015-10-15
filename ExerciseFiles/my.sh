e97fdd5cd92ec0b0b673067532000830ef9ce0b5
Michelle Munksgard <design@michellemunksgard.com>
2015-10-15 01:42:11 -0400

    Coloring and Styling Text / Uploaded Notes

--- ExerciseFiles/my.sh
+++ ExerciseFiles/my.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+flashingred=$(tput setab 0; tput setaf 1; tput blink)
+red=$(tput setab 0; tput setaf 1)
+none=$(tput sgr0)
+echo -e $flashingred"ERROR: "$none$red"Something went wrong."$none
