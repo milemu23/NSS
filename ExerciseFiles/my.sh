40f772e02954f5eea83cf35be08f451a769169f4
Michelle Munksgard <design@michellemunksgard.com>
2015-10-15 01:23:56 -0400

    Working with Strings

--- ExerciseFiles/my.sh
+++ ExerciseFiles/my.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-[[ "cat" == "cat" ]]
-echo $?
 
-[[ "cat" == "dog" ]]
-echo $?
-
-[[ 20 > 100 ]]
-echo $?
-
-[[ 20 -gt 100 ]]
-echo $?
-
-a=""
-b="cat"
-[[ -z $a && -n $b ]]
-echo $?
