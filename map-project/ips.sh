4a5281d4924c9288b60197f8c963f964e25ce300
milemu23 <Munk13@Michelles-MacBook-Pro.local>
2015-10-22 23:55:22 -0400

    Fixed son error

--- map-project/ips.sh
+++ map-project/ips.sh
@@ -6,7 +6,7 @@ myips=($(cat ipAdd.txt | awk '{print $2}'))
 
 let i=0
 while (( ${#myips[@]} > i)); do
-	printf -v d "{\n\"ipaddress\":\t\"${myips[i++]}\",\n},\n"
+	printf -v d "{\n\"ipaddress\":\t\"${myips[i++]}\",\n}\n"
 echo "$d"
 
 done
