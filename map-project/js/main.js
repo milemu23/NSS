6bb76c83753fbe298d4f967744d0d78f23d18c8e
Michelle Munksgard <design@michellemunksgard.com>
2015-10-27 18:53:36 -0400

    Added json file

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -4,6 +4,11 @@
    $(function(){
         try{
             IPMapper.initializeMap("map");
+            
+            var ipArray = $.getJSON('ipaddresses.json', function(data){
+                console.log(data);   
+            }
+            
             IPMapper.addIPMarker("111.111.111.111");
         } catch(e){
             //handle error
