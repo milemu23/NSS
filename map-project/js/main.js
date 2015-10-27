b04cc94f85d4c36e41f62bc1a422da10e49d0ed8
Michelle Munksgard <design@michellemunksgard.com>
2015-10-27 19:27:59 -0400

    change json

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -6,7 +6,7 @@
             IPMapper.initializeMap("map");
             
             var ipArray = JSON.parse(data);
-            console.log(ipaddress[2]);
+            console.log(ipaddress[0]);
         
             
             IPMapper.addIPMarker("111.111.111.111");
