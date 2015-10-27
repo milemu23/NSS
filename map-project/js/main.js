84fcc6155faf25b7f2d8438030cefa8588d5c966
Michelle Munksgard <design@michellemunksgard.com>
2015-10-27 18:59:13 -0400

    ipaddress

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -6,7 +6,7 @@
             IPMapper.initializeMap("map");
             
             var ipArray = JSON.parse(data);
-            console.log(location[2]);
+            console.log(ipaddress[2]);
         
             
             IPMapper.addIPMarker("111.111.111.111");
