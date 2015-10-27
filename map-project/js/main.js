b87f450fd007f67f29e414a65c8eee28ef1cf86f
Michelle Munksgard <design@michellemunksgard.com>
2015-10-27 18:47:32 -0400

    test

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -1,16 +1,10 @@
 //Michelle Munksgard
 //Javascript Map
 
-$(function(){
-    try{
-        IPMapper.initializeMap("map");
-        
-		var ipArray = $.getJSON('ipaddresses.json', function(data){
-                console.log(data);
-                                        ipArray = (data);adfasdf
-                
-            });
-        IPMapper.addIPArray(ipArray);
-    } catch(e){
-    }
-});
+   $(function(){
+        try{
+            IPMapper.initializeMap("map");
+            IPMapper.addIPMarker("111.111.111.111");
+        } catch(e){
+            //handle error
+        }
