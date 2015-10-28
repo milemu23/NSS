a25f3991847d3ff662b5b239f6afe478f6031c8e
milemu23 <Munk13@Michelles-MacBook-Pro.local>
2015-10-27 23:26:09 -0400

    test

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -7,7 +7,7 @@
            
            $.getJSON("ipaddresses.json", function( data ){
                
-           console.log(data.ipaddresses.ipaddress);
+           console.log(data.ipaddress);
            });
            
            for (var i = 0; i < ipArray.length; i++) {
