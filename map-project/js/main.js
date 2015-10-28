49e0beed45ccc78d705655e4f04206bbfe7bd0e9
milemu23 <Munk13@Michelles-MacBook-Pro.local>
2015-10-27 22:09:42 -0400

    testing

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -4,15 +4,14 @@
 	   "use strict";
 	   try{
             IPMapper.initializeMap("map");
-           console.log("Hello!");
+           
             
             $.ajax({
                 url: "ipaddresses.json",
                 dataType: "json",
                 success: function(data) {
                     for (var i = 0; i < data.length; i++){
-                         var ipAdd = JSON.parse(data);
-                        IPMapper.addIPArray(ipAdd.ipaddress);
+                        console.log("Hello!");
                     }
 			
         } 
