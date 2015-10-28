27ea694eed32f063bd4b3daefcdb0ebcd1f883aa
milemu23 <Munk13@Michelles-MacBook-Pro.local>
2015-10-27 21:42:07 -0400

    Testing changes

---
+++ cron-assignment/cron2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#add prefix
+mkdir -p archive
+
+cp -r /Users/Munk13/Desktop/NSS/cron-assignment/old/* /Users/Munk13/Desktop/NSS/cron-assignment/archive
+
+cd /Users/Munk13/Desktop/NSS/cron-assignment/archive
+
+for filename in $ARCHIVE/ *; do mv "$filename" "old_$filename";
+done
+
+/bin/bash /Users/Munk13/Desktop/NSS/cron-assignment/old/old_ips.sh
