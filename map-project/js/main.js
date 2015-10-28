88fb418c503cfba8ff20171e50e7b12f77b0e82a
milemu23 <Munk13@Michelles-MacBook-Pro.local>
2015-10-27 22:49:04 -0400

    testing again

--- map-project/js/main.js
+++ map-project/js/main.js
@@ -2,24 +2,15 @@
 //Javascript Map
    $(document).ready(function(){
 	   "use strict";
-	  
+	   try {
             IPMapper.initializeMap("map");
            
-            
-            $.ajax({
-                url: 'ipaddresses.json',
-                data: { get_param: 'value' },
-                dataType: 'json',
-                success: function(ipaddresses) {
-                    
-                    console.log("Success!");
-                    var ipArray = $.parseJSON(ipaddresses);
-                    for (var i = 0; i < ipArray.length; i++){
-                        ipArray[i].ipaddress;
-                    }
-        } 
-		})
-		 	
-			
-			
-		 });
+            var ipArray = JSON.parse(data);
+           for (var i = 0; i < ipArray.ipaddresses.length; i++) {
+               var ips = ipArray.ipaddresses[i];
+               IPMapper.addIPArray(ips);
+           
+       } catch(e){
+           
+       }    
+   });
